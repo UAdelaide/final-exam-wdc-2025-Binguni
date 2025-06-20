5dc9dd918d11f6241eeae6b673dca46ba79fcfb0
Binguni <a1876390@adelaide.edu.au>
2025-06-20 23:52:52 +0000

    [automatic commit] 2025-06-20 23:52:52 UTC 123.243.92.73

---
+++ part1/sql_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Users (username, email, password_hash, role)
+VALUES
+  ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+  ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+  ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+  ('dave456', 'dave@example.com', 'hashed321', 'owner'),
+  ('emma789', 'emma@example.com', 'hashed654', 'walker');
+
+  
