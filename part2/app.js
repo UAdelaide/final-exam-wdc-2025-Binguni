b0b008d5101f03ca7d590b95817c31094e010311
Binguni <a1876390@adelaide.edu.au>
2025-06-21 06:58:12 +0000

    [automatic commit] 2025-06-21 06:58:12 UTC 123.243.92.73

--- part2/app.js
+++ part2/app.js
@@ -42,7 +42,11 @@ app.post('/login', async (req, res) => {
 
 
 app.post('/logout', (req, res) => {
-    req.session.destroy
+    req.session.destroy(err => {
+        if (err){
+            return res.status(500)
+        }
+    })
 })
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
