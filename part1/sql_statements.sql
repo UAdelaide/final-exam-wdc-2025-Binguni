3ab6b1c3e1730f618de167281f496406510d5548
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:17:39 +0000

    [automatic commit] 2025-06-21 00:17:39 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -22,7 +22,10 @@ VALUES
      (SELECT user_id FROM Users WHERE username = 'carol123')),
 
 
-     INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
+     INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes,location, status)
+     VALUES
+
+     (())
 
 
 
