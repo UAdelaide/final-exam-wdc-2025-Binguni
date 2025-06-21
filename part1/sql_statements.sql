f48db73e52463623ba37c7e773fecd77bbfa1370
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:21:29 +0000

    [automatic commit] 2025-06-21 00:21:29 UTC 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -30,7 +30,7 @@ VALUES
      ((select dog_id FROM Dogs WHERE name= 'Bella')
      ' 2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
      ((select dog_id FROM Dogs WHERE name= 'Lando')
-     ' 2025-06-10 08:00:00', 30, 'Patklands', 'open'),
+     ' 2025-06-11 10:30:00', 30, 'Patklands', 'open'),
      ((select dog_id FROM Dogs WHERE name= 'Max')
      ' 2025-06-10 08:00:00', 30, 'Patklands', 'open'),
      ((select dog_id FROM Dogs WHERE name= 'Max')
