3d0adf22a549aef4c9cfea8b8171780074a38aa9
Binguni <a1876390@adelaide.edu.au>
2025-06-21 05:38:59 +0000

    [automatic commit] 2025-06-21 05:38:59 UTC 123.243.92.73

--- part2/app.js
+++ part2/app.js
@@ -13,7 +13,7 @@ app.use(session({
     saveUninitialized: true
 }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 
