77793bbebb90d769e9208f4ef0383c92699dd57a
Binguni <a1876390@adelaide.edu.au>
2025-06-21 01:17:02 +0000

    [automatic commit] 2025-06-21 01:17:02 UTC 123.243.92.73

--- part1/app.js
+++ part1/app.js
@@ -15,7 +15,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '' // Set your MySQL root password
-      database
+      database: 'dogwalks'
     });
 
     // Create the database if it doesn't exist
