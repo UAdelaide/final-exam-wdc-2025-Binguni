b471fc61368a26180b3e3c0b63368062ea99659a
Binguni <a1876390@adelaide.edu.au>
2025-06-21 02:40:05 +0000

    [automatic commit] 2025-06-21 02:40:05 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/app.js
+++ part1/app.js
@@ -4,7 +4,7 @@ var mysql = require('mysql2/promise');
 const app = express();
 let db= null;
 
-async fucntion init () => {
+async fucntion initDb() {
   try {
     // Connect to MySQL database
     db = await mysql.createConnection({
