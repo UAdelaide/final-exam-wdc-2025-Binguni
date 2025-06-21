8eea1c8c9785de939cb41e0aecfa11a40802b271
Binguni <a1876390@adelaide.edu.au>
2025-06-21 04:05:59 +0000

    [automatic commit] 2025-06-21 04:05:59 UTC 123.243.92.73

--- part1/app.js
+++ part1/app.js
@@ -1,30 +1,31 @@
-var express = require('express');
-const path= require ('path');
-
-
-app.use(express.static(path,join(__dirname, 'public')));
-var mysql = require('mysql2/promise');
+const express = require('express');
+const path = require('path');
+const mysql = require('mysql2/promise');
 
 const app = express();
-let db= null;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+let db = null;
+
 
 (async () => {
   try {
-    // Connect to MySQL database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '', // Set your MySQL root password
+      password: '', // Use your MySQL password here
       database: 'DogWalkService'
     });
-
     console.log('Connected to database');
   } catch (err) {
     console.error('Failed to connect to database:', err.message);
   }
 })();
+
 function getDb() {
-    return db;
+  return db;
 }
 
 module.exports = { app, getDb };
