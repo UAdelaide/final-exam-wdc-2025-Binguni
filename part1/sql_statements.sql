bc2bb0f1f6e3e9480d3208cf01849b254c3e4deb
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:05:48 +0000

    [automatic commit] 2025-06-21 00:05:48 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -2,4 +2,4 @@ INSERT INTO Users (uername, email, password_hash, role)
 VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner')
   ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-  ('carol')
+  ('carol@example.com', 'hashed789', 'owner')
