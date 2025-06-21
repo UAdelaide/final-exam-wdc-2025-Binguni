e3bfe54aaffda3971545e6a102685521f967cef6
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:24:39 +0000

    [automatic commit] 2025-06-21 00:24:39 UTC 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -33,8 +33,8 @@ VALUES
      ' 2025-06-11 10:30:00', 60, 'Botanic Gardens', 'open'),
      ((select dog_id FROM Dogs WHERE name= 'Oscar'),
      ' 2025-06-10 11:15:00', 30, 'North Terrace, 'cancelled'),
-     ((select dog_id FROM Dogs WHERE name= 'Liam')
-     ' 2025-06-11 13:00:00', 25, 'Findon', 'completed'),
+     ((select dog_id FROM Dogs WHERE name= 'Liam'),
+     ' 2025-06-11 13:00:00', 25, 'Findon', 'completed');
 
 
 
