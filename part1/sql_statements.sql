5bab100616d629885190d9ab43010aa814be95d2
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:33:10 +0000

    [automatic commit] 2025-06-21 00:33:10 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -30,11 +30,11 @@ VALUES
      ((select dog_id FROM Dogs WHERE name= 'Bella'),
      '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
      ((select dog_id FROM Dogs WHERE name= 'Lando'),
-     ' 2025-06-11 10:30:00', 60, 'Botanic Gardens', 'open'),
+     '2025-06-11 10:30:00', 60, 'Botanic Gardens', 'open'),
      ((select dog_id FROM Dogs WHERE name= 'Oscar'),
-     ' 2025-06-10 11:15:00', 30, 'North Terrace', 'cancelled'),
+     '2025-06-10 11:15:00', 30, 'North Terrace', 'cancelled'),
      ((select dog_id FROM Dogs WHERE name= 'Liam'),
-     ' 2025-06-11 13:00:00', 25, 'Findon', 'completed');
+     '2025-06-11 13:00:00', 25, 'Findon', 'completed');
 
 
 
