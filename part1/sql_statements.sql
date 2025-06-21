2ddb557e100a798f92e4e93e9642334abf3c50c9
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:28:18 +0000

    [automatic commit] 2025-06-21 00:28:18 UTC 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -5,7 +5,7 @@ VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner')
   ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
   ('carol','carol@example.com', 'hashed789', 'owner')
-  ('alex2025', 'alex@example.com', 'hashed999'. 'owner')
+  ('alex2025', 'alex@example.com', 'hashed999', 'owner')
   ('george456', 'george@example.com', 'hashed111', 'walker');
 
   INSERT INTO Dogs (name, size, owner_id)
