1260502ae858010987d0bab0f141a92020ba5afc
Binguni <a1876390@adelaide.edu.au>
2025-06-21 03:35:58 +0000

    [automatic commit] 2025-06-21 03:35:58 UTC 123.243.92.73

--- part1/index.js
+++ part1/index.js
@@ -46,30 +46,31 @@ app.get('/api/dogs', async (req, res) => {
     }
   });
 
-  app.get('/api/walkers/summary' , async (req, res ) => {
-const db= getDb();
-if (!db) {
-    return res.status(500).json({ error: 'Database not ready'});
-}
-try {
-    const [rows] = await db.execute (`
+  app.get('/api/walkers/summary', async (req, res) => {
+    const db = getDb();
+    if (!db) {
+      return res.status(500).json({ error: 'Database not ready' });
+    }
+
+    try {
+      const [rows] = await db.execute(`
         SELECT
-       Users.username AS walker_name,
-       COUNT(WalkApplications.request_id) AS total_completed_walks,
-       AVG(WalkRatings.rating) AS average_rating
-       FROM WalkApplications
-       JOIN Users on WalkApplications.walker_id= Users.user_id
-       JOIN WalkRequests ON WalkApplications.request_id= WalkRequests.request_id
-       LEFT JOIN WalkRatings ON WalkRequests.request_id = WalkRatings.request_id
-       WHERE WalkRequests.status = 'completed'
-       AND Users.role = 'walker'
-       GROUP BY Users.user_id;
-`);
-        res.json(rows);
+          Users.username AS walker_name,
+          COUNT(WalkApplications.request_id) AS total_completed_walks,
+          AVG(WalkRatings.rating) AS average_rating
+        FROM WalkApplications
+        JOIN Users ON WalkApplications.walker_id = Users.user_id
+        JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id
+        LEFT JOIN WalkRatings ON WalkRequests.request_id = WalkRatings.request_id
+        WHERE WalkRequests.status = 'completed'
+          AND Users.role = 'walker'
+        GROUP BY Users.user_id;
+      `);
+      res.json(rows);
     } catch (err) {
       console.error('Error fetching walk summary:', err.message);
       res.status(500).json({ error: 'Failed to fetch walker summary' });
-}
+    }
   });
 
 
