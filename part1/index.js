d9013f0881c0ab2d53ca47450463597859955f4a
Binguni <a1876390@adelaide.edu.au>
2025-06-21 02:57:52 +0000

    [automatic commit] 2025-06-21 02:57:52 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/index.js
+++ part1/index.js
@@ -21,6 +21,8 @@ app.get('/api/dogs', async (req, res) => {
   });
 
   app.get('/api/walkrequests/open', async (req, res) => {
+    const db= getDb();
+    if (!)
     try {
       const [rows] = await db.execute(`
         SELECT
