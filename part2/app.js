3b03102747c39055c1f9d0083cc48161268c3a40
Binguni <a1876390@adelaide.edu.au>
2025-06-21 07:02:00 +0000

    [automatic commit] 2025-06-21 07:02:00 UTC 123.243.92.73

--- part2/app.js
+++ part2/app.js
@@ -44,7 +44,7 @@ app.post('/login', async (req, res) => {
 app.post('/logout', (req, res) => {
     req.session.destroy(err => {
         if (err){
-            return res.status(500).json({ error:'logout'
+            return res.status(500).json({ error:'logout failed'});
             }
             res.clearCookie('connect.sid');
             res.sendStatus(200);
