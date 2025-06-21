371f21377e66213a48d1898154d4e1a964f05da9
Binguni <a1876390@adelaide.edu.au>
2025-06-21 05:25:44 +0000

    [automatic commit] 2025-06-21 05:25:44 UTC 123.243.92.73

--- part2/app.js
+++ part2/app.js
@@ -19,7 +19,10 @@ const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
 app.post('/login', async (req, res) => {
-    const {username, password } = req.body
+    const {username, password } = req.body;
+    try {
+        const [users]
+    }
 })
 
 app.use('/api/walks', walkRoutes);
