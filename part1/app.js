9083933aa6b5f884583bf22035f5097668356705
Binguni <a1876390@adelaide.edu.au>
2025-06-21 01:10:37 +0000

    [automatic commit] 2025-06-21 01:10:37 UTC 123.243.92.73

--- part1/app.js
+++ part1/app.js
@@ -3,11 +3,9 @@ var express = require('express');
 var mysql = require('mysql2/promise');
 
 const app = express();
+app.use (express.json());
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 
 let db;
 
