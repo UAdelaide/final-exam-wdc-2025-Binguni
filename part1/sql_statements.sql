23e35ca7f1ec40dd4b556fbf3660f3074e60f71f
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:24:19 +0000

    [automatic commit] 2025-06-21 00:24:18 UTC 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -25,13 +25,13 @@ VALUES
      INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes,location, status)
      VALUES
 
-     ((select dog_id FROM Dogs WHERE name= 'Max')
+     ((select dog_id FROM Dogs WHERE name= 'Max'),
      ' 2025-06-10 08:00:00', 30, 'Patklands', 'open'),
-     ((select dog_id FROM Dogs WHERE name= 'Bella')
+     ((select dog_id FROM Dogs WHERE name= 'Bella'),
      ' 2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-     ((select dog_id FROM Dogs WHERE name= 'Lando')
+     ((select dog_id FROM Dogs WHERE name= 'Lando'),
      ' 2025-06-11 10:30:00', 60, 'Botanic Gardens', 'open'),
-     ((select dog_id FROM Dogs WHERE name= 'Oscar')
+     ((select dog_id FROM Dogs WHERE name= 'Oscar'),
      ' 2025-06-10 11:15:00', 30, 'North Terrace, 'cancelled'),
      ((select dog_id FROM Dogs WHERE name= 'Liam')
      ' 2025-06-11 13:00:00', 25, 'Findon', 'completed'),
