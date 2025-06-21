578047c70bcaecf6b166b52b83590cbaf67c8e30
Binguni <a1876390@adelaide.edu.au>
2025-06-21 05:27:52 +0000

    [automatic commit] 2025-06-21 05:27:52 UTC 123.243.92.73

--- part2/app.js
+++ part2/app.js
@@ -22,7 +22,8 @@ app.post('/login', async (req, res) => {
     const {username, password } = req.body;
     try {
         const [users] = await db.query('SELECT * FROM Users WHERE username= ?'), [username]);
-        req.session.user = { id: user.user_id}
+        req.session.user = { id: user.user_id, role: user.role};
+        res.json({})
     }
 })
 
