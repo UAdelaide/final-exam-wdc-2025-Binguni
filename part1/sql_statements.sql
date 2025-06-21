9e588175d55b297b2258da0b7d0885410687e028
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:06:30 +0000

    [automatic commit] 2025-06-21 00:06:30 UTC 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -3,4 +3,4 @@ VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner')
   ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
   ('carol','carol@example.com', 'hashed789', 'owner')
-  ('')
+  ('alex2025', 'alex')
