9f147e28029833a7be30791ba1dc76e4d6dae1b6
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:33:57 +0000

    [automatic commit] 2025-06-21 00:33:57 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -2,10 +2,10 @@
 
 INSERT INTO Users (username, email, password_hash, role)
 VALUES
-  ('alice123', 'alice@example.com', 'hashed123', 'owner')
-  ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
-  ('carol123','carol@example.com', 'hashed789', 'owner')
-  ('alex2025', 'alex@example.com', 'hashed999', 'owner')
+  ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+  ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+  ('carol123','carol@example.com', 'hashed789', 'owner'),
+  ('alex2025', 'alex@example.com', 'hashed999', 'owner'),
   ('george456', 'george@example.com', 'hashed111', 'walker');
 
   INSERT INTO Dogs (name, size, owner_id)
