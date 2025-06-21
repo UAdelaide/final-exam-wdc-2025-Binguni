2d5e4d8ed7580d3fce4e16b2d4807c9008859c08
Binguni <a1876390@adelaide.edu.au>
2025-06-21 01:32:20 +0000

    [automatic commit] 2025-06-21 01:32:20 UTC 123.243.92.73

--- part1/app.js
+++ part1/app.js
@@ -8,17 +8,18 @@ let db;
 
 (async () => {
   try {
-    // Connect to MySQL without specifying a database
+    // Connect to MySQL database
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '',// Set your MySQL root password
+      password: '', // Set your MySQL root password
       database: 'dogwalks'
     });
 
     console.log('Connected to database');
   } catch (err) {
-    console.error('Failed to connect to databse:', err.message);
-  })();
- module.exports = { app, db };
+    console.error('Failed to connect to database:', err.message);
+  }
+})(); // ← This was missing a closing parenthesis!
 
+module.exports = { app, db };
