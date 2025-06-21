3c71657ccf7fa736805b866fcfd664235110fdb2
Binguni <a1876390@adelaide.edu.au>
2025-06-21 00:05:17 +0000

    [automatic commit] 2025-06-21 00:05:17 UTC ;; communications error to 208.67.222.222#53: timed out 123.243.92.73

--- part1/sql_statements.sql
+++ part1/sql_statements.sql
@@ -1,4 +1,5 @@
 INSERT INTO Users (uername, email, password_hash, role)
 VALUES
   ('alice123', 'alice@example.com', 'hashed123', 'owner')
-  ('bobwalker', 'bob@example.com', 'hashed456')
+  ('bobwalker', 'bob@example.com', 'hashed456', 'walker')
+  ('carol')
